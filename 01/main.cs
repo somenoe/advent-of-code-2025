d89d2f15ab24c62c70d94187fd46e0ae20750cf2
somenoe <otashi24@gmail.com>
2025-12-03 05:54:24 +0700

    right answer, weird process

--- 01/main.cs
+++ 01/main.cs
@@ -1,7 +1,7 @@
 const string EXAMPLE_FILE_PATH = "example.txt";
 const string INPUT_FILE_PATH = "input.txt";
 const int INITIAL_DIAL_VALUE = 50;
-const int MAX_DIAL_VALUE = 99;
+const int DIAL_POSITIONS = 100;
 
 Console.WriteLine($@"
 Constants:
@@ -10,32 +10,55 @@ Constants:
     START_AT          = {INITIAL_DIAL_VALUE}
 ");
 
+string selectedFile = EXAMPLE_FILE_PATH;
+if (args.Length > 0 && args[0] == "i") selectedFile = INPUT_FILE_PATH;
+
 int dial = INITIAL_DIAL_VALUE;
 int zeroPointCounter = 0;
-string[] lines = File.ReadAllLines(INPUT_FILE_PATH);
+string[] lines = File.ReadAllLines(selectedFile);
 
 foreach (string line in lines)
 {
     char rotationType = line[0];
     string rotateString = new(line.Where(char.IsDigit).ToArray());
     int rotationAngle = int.Parse(rotateString);
-    Console.WriteLine($"{rotationType} {rotationAngle}\t#{dial}");
+    Console.WriteLine($"{rotationType} {rotationAngle}\t# {dial}");
 
     switch (rotationType)
     {
         case 'L':
             dial -= rotationAngle;
-            if (dial < 0) dial = MAX_DIAL_VALUE + 1 + dial;
             break;
         case 'R':
             dial += rotationAngle;
-            if (dial > MAX_DIAL_VALUE) dial -= MAX_DIAL_VALUE + 1;
             break;
         default:
             throw new Exception($"Unknow rotation type: {rotationType}");
     }
 
+    dial %= DIAL_POSITIONS;
+
     if (dial == 0) zeroPointCounter++;
 }
 
-Console.WriteLine($"Actual password: {zeroPointCounter}");
+Console.WriteLine($"\nActual password: {zeroPointCounter}");
+
+/*
+This verison get right answer but it got negative dial:
+
+L 68    # 50
+L 30    # -18
+R 48    # -48
+L 5     # 0
+R 60    # -5
+L 55    # 55
+L 1     # 0
+L 99    # -1
+R 14    # 0
+L 82    # 14
+
+Actual password: 3
+
+beccause of this line: `dial %= DIAL_POSITIONS;`
+`%` in C# is not modulo, it's REMAINDER!
+*/
