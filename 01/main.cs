8733fe2cb6db2a3a451b001f96782b2148aeb385
somenoe <otashi24@gmail.com>
2025-12-03 05:51:09 +0700

    pass example, wrong answer

--- 01/main.cs
+++ 01/main.cs
@@ -1,20 +1,41 @@
 const string EXAMPLE_FILE_PATH = "example.txt";
 const string INPUT_FILE_PATH = "input.txt";
-const int START_AT = 50;
+const int INITIAL_DIAL_VALUE = 50;
+const int MAX_DIAL_VALUE = 99;
 
 Console.WriteLine($@"
 Constants:
     EXAMPLE_FILE_PATH = {EXAMPLE_FILE_PATH}
     INPUT_FILE_PATH   = {INPUT_FILE_PATH}
-    START_AT          = {START_AT}
+    START_AT          = {INITIAL_DIAL_VALUE}
 ");
 
-string[] lines = File.ReadAllLines(EXAMPLE_FILE_PATH);
+int dial = INITIAL_DIAL_VALUE;
+int zeroPointCounter = 0;
+string[] lines = File.ReadAllLines(INPUT_FILE_PATH);
 
 foreach (string line in lines)
 {
     char rotationType = line[0];
     string rotateString = new(line.Where(char.IsDigit).ToArray());
     int rotationAngle = int.Parse(rotateString);
-    Console.WriteLine($"{rotationType} {rotationAngle}");
+    Console.WriteLine($"{rotationType} {rotationAngle}\t#{dial}");
+
+    switch (rotationType)
+    {
+        case 'L':
+            dial -= rotationAngle;
+            if (dial < 0) dial = MAX_DIAL_VALUE + 1 + dial;
+            break;
+        case 'R':
+            dial += rotationAngle;
+            if (dial > MAX_DIAL_VALUE) dial -= MAX_DIAL_VALUE + 1;
+            break;
+        default:
+            throw new Exception($"Unknow rotation type: {rotationType}");
+    }
+
+    if (dial == 0) zeroPointCounter++;
 }
+
+Console.WriteLine($"Actual password: {zeroPointCounter}");
