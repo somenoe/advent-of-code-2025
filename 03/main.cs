2f4fee0848303abf5a8fcb782c058cc90986d6a6
somenoe <otashi24@gmail.com>
2025-12-04 00:36:31 +0700

    pass day 3

--- 03/main.cs
+++ 03/main.cs
@@ -6,8 +6,36 @@ if (args.Length > 0 && args[0] == "i") selectedFile = INPUT_FILE_PATH;
 
 string[] lines = File.ReadAllLines(selectedFile);
 
-foreach (string line in lines)
+int totalOutputJoltage = 0;
+
+int[][] joltageRatings = lines.Select(line => line.Select(c => int.Parse(c.ToString())).ToArray()).ToArray();
+
+foreach (int[] line in joltageRatings)
 {
-    Console.WriteLine(line);
+    int first = 0;
+    int second = 0;
+
+    for (int i = 0; i < line.Length - 1; i++)
+    {
+        int currentJoltage = line[i];
+        if (currentJoltage > first)
+        {
+            first = currentJoltage;
+            second = 0;
+        }
+        else if (currentJoltage > second)
+        {
+            second = currentJoltage;
+        }
+
+    }
+    if (line[^1] > second)
+    {
+        second = line[^1];
+    }
+    Console.WriteLine($"{string.Join("", line)} - {first} {second}");
+
+    totalOutputJoltage += first * 10 + second;
 }
 
+Console.WriteLine($"Total output Joltage: {totalOutputJoltage}");
