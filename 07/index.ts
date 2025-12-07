da795c1dded6d34d520738d502114e69c879612c
somenoe <otashi24@gmail.com>
2025-12-07 23:53:30 +0700

    add day 7 puzzle

---
+++ 07/index.ts
@@ -0,0 +1,16 @@
+const DEBUG = true;
+
+function print(...param: any) {
+  console.log(...param);
+}
+function debug(...param: any) {
+  if (DEBUG) print(...param);
+}
+
+const inputFilePath = Bun.argv[2] == "i" ? "input.txt" : "example.txt";
+const input = Bun.file(inputFilePath);
+
+const content = await input.text();
+const lines = content.split(/\r?\n/);
+
+debug(lines.join("\n"));
